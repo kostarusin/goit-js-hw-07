9713c9465669e3f5a8d6d60c25d9c1e48b92f926
Kostyantyn <kosta.rusin@gmail.com>
2023-05-16 12:29:09 +0200

    1

--- js/task-05.js
+++
@@ -1,7 +0,0 @@
-const nameInput = document.querySelector('#name-input');
-const nameOutput = document.querySelector('#name-output');
-
-nameInput.addEventListener('input', () => {
-  const inputValue = nameInput.value.trim();
-  nameOutput.textContent = inputValue !== '' ? inputValue : 'Anonymus';
-});
